6d8252f7affefcb6cfac5fe0ca46b81a913809fb
azaddunite <azad.hossain@dunite.se>
2024-07-12 14:26:24 +0600

    modify web apis

--- src/adapters/controllers/screenshotController.ts
+++ src/adapters/controllers/screenshotController.ts
@@ -1,51 +1,50 @@
 import fs from 'fs';
 import { PassThrough } from 'stream';
 
-import { Request,Response } from "express";
-import puppeteer from 'puppeteer';
+import { Request, Response } from "express";
+import puppeteer, { Browser, Page } from 'puppeteer';
 
 import { PuppeteerScreenRecorder } from '../../lib/PuppeteerScreenRecorder';
 
-class ScreenshotController
-{
-    
-    public async stop(req:Request,res:Response)
-    {
-        const executablePath = process.env['PUPPETEER_EXECUTABLE_PATH'];
-        const browser = await puppeteer.launch({...(executablePath ? { executablePath: executablePath } : {}), headless: false, });
-        const page = await browser.newPage();
-        const recorder = new PuppeteerScreenRecorder(page);
-        await recorder.stop();
-        await browser.close();
+class ScreenshotController {
+    public browser: Browser;
+    public page: Page;    
+    public recorder:PuppeteerScreenRecorder;
+    public recording: boolean = false;
+
+    public async stop(req: Request, res: Response) {
+        if (this.recording) {             
+            await this.recorder.stop();
+            await this.browser.close();
+        }
+
+        this.recording = false;
         res.send('Stoped');
     }
 
-    public status(req:Request,res:Response)
-    {
-       const isRunning=false;
-       if(!isRunning){
-        res.send(`Not running..`);
-       }
-       else
-       {
-        res.send(`Running..`);
-       }
-         
-       
+    public status(req: Request, res: Response) {
+
+        if (!this.recording) {
+            res.send(`Not running..`);
+        }
+        else {
+            res.send(`Running..`);
+        }
+
+
     }
-    public async start(req:Request,res:Response)
-    {
-        const path=req.query.path;
+    public async start(req: Request, res: Response) {
+        const path = req.query.path;
         console.log(path);
-        
-        let format='./report/video/simple1.mp4';
+
+        let format = './report/video/simple1.mp4';
         const argList = process.argv.slice(2);
         const isStream = argList.includes('stream');
         console.log(`Testing with Method using ${isStream ? 'stream' : 'normal'} mode`);
         const executablePath = process.env['PUPPETEER_EXECUTABLE_PATH'];
-        const browser = await puppeteer.launch({...(executablePath ? { executablePath: executablePath } : {}), headless: false, });
-        const page = await browser.newPage();
-        const recorder = new PuppeteerScreenRecorder(page);
+        this.browser = await puppeteer.launch({ ...(executablePath ? { executablePath: executablePath } : {}), headless: false, });
+        this.page = await this.browser.newPage();
+        this.recorder = new PuppeteerScreenRecorder(this.page);
 
         if (isStream) {
             console.log(`stream`);
@@ -53,20 +52,22 @@ class ScreenshotController
             format = format.replace('video', 'stream');
             const fileWriteStream = fs.createWriteStream(format);
             passthrough.pipe(fileWriteStream);
-            await recorder.startStream(passthrough);
-            
+            await this.recorder.startStream(passthrough);
+
         } else {
             console.log(`recorded`);
-            await recorder.start(format);
+            await this.recorder.start(format);
         }
 
-        await page.goto(`https://ai-producer-gfx.web.app/p/CQ1J7IJ1/output`);
-        await page.setViewport({ width: 1920, height: 1080 });
+        await this.page.goto(`https://ai-producer-gfx.web.app/p/CQ1J7IJ1/output`);
+        await this.page.setViewport({ width: 1920, height: 1080 });
 
         await new Promise(r => setTimeout(r, 15000));
         //await page.waitFor(10 * 1000);
-        await recorder.stop();
-        await browser.close();
+        this.recording = true;
+        // await recorder.stop();
+        // await this.browser.close();
+
         res.send(`Capture Done.`);
     }
 }
--- src/routes/screenshotRoute.ts
+++ src/routes/screenshotRoute.ts
@@ -4,7 +4,7 @@ import ScreenshotController from "../adapters/controllers/screenshotController";
 
 const router= Router();
 
-router.get('/screenShot',ScreenshotController.start.bind(ScreenshotController));
+router.get('/screenShot/start',ScreenshotController.start.bind(ScreenshotController));
 router.get('/screenShot/stop',ScreenshotController.stop.bind(ScreenshotController));
 router.get('/screenShot/status',ScreenshotController.status.bind(ScreenshotController));
 // router.post('/screenShot',ScreenshotController.create.bind(ScreenshotController));
