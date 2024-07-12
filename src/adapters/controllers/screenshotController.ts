0743b442e3bfc4fd075b88b63b47d102123fe189
azaddunite <azad.hossain@dunite.se>
2024-07-12 18:14:39 +0600

    modify code

--- src/adapters/controllers/screenshotController.ts
+++ src/adapters/controllers/screenshotController.ts
@@ -8,67 +8,73 @@ import { PuppeteerScreenRecorder } from '../../lib/PuppeteerScreenRecorder';
 
 class ScreenshotController {
     public browser: Browser;
-    public page: Page;    
-    public recorder:PuppeteerScreenRecorder;
+    public page: Page;
+    public recorder: PuppeteerScreenRecorder;
     public recording: boolean = false;
 
     public async stop(req: Request, res: Response) {
-        if (this.recording) {             
+        if (this.recording) {
             await this.recorder.stop();
             await this.browser.close();
+            this.recording = false;
+            res.send('Recording stopped.');
+        } else {
+            res.send('Recording not started.');
         }
 
-        this.recording = false;
-        res.send('Stoped');
     }
 
     public status(req: Request, res: Response) {
 
         if (!this.recording) {
-            res.send(`Not running..`);
+            res.send(`Not recorded.`);
         }
         else {
-            res.send(`Running..`);
+            res.send(`Recorded.`);
         }
 
 
     }
     public async start(req: Request, res: Response) {
         const path = req.query.path;
-        console.log(path);
-
         let format = './report/video/simple1.mp4';
         const argList = process.argv.slice(2);
         const isStream = argList.includes('stream');
         console.log(`Testing with Method using ${isStream ? 'stream' : 'normal'} mode`);
         const executablePath = process.env['PUPPETEER_EXECUTABLE_PATH'];
-        this.browser = await puppeteer.launch({ ...(executablePath ? { executablePath: executablePath } : {}), headless: false, });
-        this.page = await this.browser.newPage();
-        this.recorder = new PuppeteerScreenRecorder(this.page);
 
-        if (isStream) {
-            console.log(`stream`);
-            const passthrough = new PassThrough();
-            format = format.replace('video', 'stream');
-            const fileWriteStream = fs.createWriteStream(format);
-            passthrough.pipe(fileWriteStream);
-            await this.recorder.startStream(passthrough);
 
+        if (!this.recording) {
+
+            this.browser = await puppeteer.launch({ ...(executablePath ? { executablePath: executablePath } : {}), headless: false, });
+            this.page = await this.browser.newPage();
+            this.recorder = new PuppeteerScreenRecorder(this.page);
+            if (isStream) {
+                const passthrough = new PassThrough();
+                format = format.replace('video', 'stream');
+                const fileWriteStream = fs.createWriteStream(format);
+                passthrough.pipe(fileWriteStream);
+                await this.recorder.startStream(passthrough);
+
+            } else {
+                console.log(`recorded`);
+                await this.recorder.start(format);
+            }
+
+            await this.page.goto(`https://ai-producer-gfx.web.app/p/CQ1J7IJ1/output`);
+            await this.page.setViewport({ width: 1920, height: 1080 });
+            res.send('Recording started.');
         } else {
-            console.log(`recorded`);
-            await this.recorder.start(format);
+            res.send('Recording running.');
         }
-
-        await this.page.goto(`https://ai-producer-gfx.web.app/p/CQ1J7IJ1/output`);
-        await this.page.setViewport({ width: 1920, height: 1080 });
-
+        this.recording = true;
         await new Promise(r => setTimeout(r, 15000));
         //await page.waitFor(10 * 1000);
-        this.recording = true;
+       
         // await recorder.stop();
         // await this.browser.close();
 
-        res.send(`Capture Done.`);
+
     }
 }
 
