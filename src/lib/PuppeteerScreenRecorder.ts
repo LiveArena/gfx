51ec9131c1b32a07bcd7c06bf28fd8fe207ef419
azaddunite <azad.hossain@dunite.se>
2024-07-27 13:09:20 +0600

    seprate images and video save folder

--- src/lib/PuppeteerScreenRecorder.ts
+++ src/lib/PuppeteerScreenRecorder.ts
@@ -69,6 +69,7 @@ export class PuppeteerScreenRecorder {
 
     let index = 1;
     const path = process.env.PNG_PATH;
+    this.ensureDirectoryExist(path);
     this.streamReader.on('pageScreenFrame', (pageScreenFrame: PageScreenFrame) => {      
       fs.writeFileSync(path + 'gfx-' + String(index++).padStart(5, "0") + '-' + pageScreenFrame.timestamp.toFixed(3).replace(".", "") + '.png', pageScreenFrame.blob);
       this.streamWriter.insert(pageScreenFrame);
